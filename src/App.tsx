205f4652053b525dd846529525be912a59b9d43a
Gabija Petroskeviciute <g.petroskeviciute@gmail.com>
2024-01-06 16:12:10 +0000

    Add initial web scripts

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+
+function App() {
+  return <div>
+            <h1> Travel Destinations </h1>
+            <p> I will share my travel itenaries here! </p>
+          </div>;
+}
+
+export default App;
